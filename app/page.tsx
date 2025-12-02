cab5f262aea57216f1e430b3711d2d60fc7ea15c
Ihor <petrovytch@gmail.com>
2025-12-02 18:54:02 +0200

    some sizes in navbar were changed

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,9 @@
 import Main from "@/components/main/main";
+import { Metadata } from 'next';
+
+export const metadata:Metadata = {
+  title: 'УНС',
+}
 
 export default function Home() {
   return (
